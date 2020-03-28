cab545e8338b7bcfa4b666fcaacb8d38c6553aac
Avula Chandra Shekar <avulachandrashekar12@gmail.com>
2020-03-28 08:01:35 +0800

    added graphql

--- app.js
+++ app.js
@@ -1,9 +1,41 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const graphqlHttp = require("express-graphql");
+const { buildSchema } = require("graphql");
 
 const app = express();
 app.use(bodyParser.json());
 const port = process.env.PORT || 3030;
+
+app.use(
+  "/graphql",
+  graphqlHttp({
+    schema: buildSchema(`
+        type rootQuery {
+            events : [String!]!
+        }
+
+        type rootMutation {
+            createEvent(name : String) : String
+        }
+
+        schema {
+            query: rootQuery
+            mutation: rootMutation
+        }
+        
+        `),
+    rootValue: {
+      events: () => {
+        return ["event1", "event2", "event3"];
+      },
+      createEvent: args => {
+        return args.name;
+      }
+    },
+    graphiql: true
+  })
+);
 
 app.get("/home", (req, res) => {
   res.statusCode = 200;
